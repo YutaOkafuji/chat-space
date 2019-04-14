1ee2b18f34728cbd2d02257f9105c133fbf9d868
ofuji <yutanoxxxuta@gmail.com>
2019-04-14 18:30:42 +0900

    Edit

--- spec/models/messages_spec.rb
+++ spec/models/messages_spec.rb
@@ -1,25 +1,26 @@
-describe  Message
+require 'rails_helper'
+
 RSpec.describe Message, type: :model do
   describe '#create' do
     context 'can save' do
-      it 'is valid with content' do
+      it 'is valid with body' do
         expect(build(:message, image: nil)).to be_valid
       end
 
       it 'is valid with image' do
-        expect(build(:message, content: nil)).to be_valid
+        expect(build(:message, body: nil)).to be_valid
       end
 
-      it 'is valid with content and image' do
+      it 'is valid with body and image' do
         expect(build(:message)).to be_valid
       end
     end
 
     context 'can not save' do
-      it 'is invalid without content and image' do
-        message = build(:message, content: nil, image: nil)
+      it 'is invalid without body and image' do
+        message = build(:message, body: nil, image: nil)
         message.valid?
-        expect(message.errors[:content]).to include('を入力してください')
+        expect(message.errors[:body]).to include('を入力してください')
       end
 
       it 'is invalid without group_id' do
@@ -27,14 +28,6 @@ RSpec.describe Message, type: :model do
         message.valid?
         expect(message.errors[:group]).to include('を入力してください')
       end
-
-      it 'is invaid without user_id' do
-        message = build(:message, user_id: nil)
-        message.valid?
-        expect(message.errors[:user]).to include('を入力してください')
-      end
-    end
-  end
 
       it 'is invaid without user_id' do
         message = build(:message, user_id: nil)
