cbbc6b155410861e33e4691b5507ec8de0c7152f
ofuji <yutanoxxxuta@gmail.com>
2019-04-14 18:26:19 +0900

    conflict

--- spec/models/messages_spec.rb
+++ spec/models/messages_spec.rb
@@ -1,35 +1,3 @@
-<<<<<<< Updated upstream
-require 'rails_helper'
-
-RSpec.describe Message, type: :model do
-  describe '#create' do
-    context 'can save' do
-      it 'is valid with content' do
-        expect(build(:message, image: nil)).to be_valid
-      end
-
-      it 'is valid with image' do
-        expect(build(:message, body: nil)).to be_valid
-      end
-
-      it 'is valid with content and image' do
-        expect(build(:message)).to be_valid
-      end
-    end
-
-    context 'can not save' do
-      it 'is invalid without content and image' do
-        message = build(:message, body: nil, image: nil)
-        message.valid?
-        expect(message.errors[:body]).to include('を入力してください')
-      end
-
-      it 'is invalid without group_id' do
-        message = build(:message, group_id: nil)
-        message.valid?
-        expect(message.errors[:group]).to include('を入力してください')
-      end
-=======
 describe  Message
 RSpec.describe Message, type: :model do
   describe '#create' do
@@ -67,7 +35,6 @@ RSpec.describe Message, type: :model do
       end
     end
   end
->>>>>>> Stashed changes
 
       it 'is invaid without user_id' do
         message = build(:message, user_id: nil)
