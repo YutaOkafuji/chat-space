4ad7e899f0d08a2b077ca8249802f42e0e4fdcef
ofuji <yutanoxxxuta@gmail.com>
2019-04-16 22:11:56 +0900

    Create ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,14 @@
 $(function(){
   $('message-form').on('submit', function(e) {
     e.preventDefault();
-    
-  })
-})
+    var formData = new FormData(this);
+    $.ajax({
+      url: '/groups/:group_id/messages',
+      type: 'POST',
+      data: formData,
+      datatype: 'json',
+      processData: false,
+      contentType: false
+    })
+  });
+});
