e57fbc9a0bc1c31e8a3f5d6748eca1a2d8134152
ofuji <yutanoxxxuta@gmail.com>
2019-04-18 00:29:00 +0900

    Message.js buildHTML

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,19 +1,41 @@
 $(function(){
+  function buildHTML(message){
+    var imagehtml = message.image == null? '' :`<img src="${message.image}" class= 'lower-message__image'>`
+    var html = `<div class="right-content__messages__message>
+                  <p class="right-content__messages__message__user-name">
+                   ${message.user_name}
+                  </p>
+                  <a class="right-content__messages__message__created_at">
+                   ${message.created_at}
+                  </a>
+                  <p class="right-content__messages__message__user-message">
+                    ${ message.body }
+                  </p>
+                  ${imagehtml}
+                </div>`
+    return html;
+  }
   $('#message-form').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
+    var url = $(this).attr('action')
     $.ajax({
-      url: '/groups/:group_id/messages',
+      url: url,
       type: 'POST',
       data: formData,
-      datatype: 'json',
+      dataType: 'json',
       processData: false,
       contentType: false
     })
-    .done(function(data){
+    .done(function(data) {
       var html = buildHTML(data);
       $('.right-content__messages').append(html);
-      $('.input-box__text').val('')
+      $('.input-box__text').val('');
+      $('.right-content__messages').animate({scrollTop:$('.right-content__messages')[0].scrollHeight});
+      $('.input-submit').attr('disabled', false);
+    })
+    .fail(function() {
+      alert('error');
     })
   });
 });
